f4bbd26cfdca09c862f82d188d4cbf8280c0b2f3
matkuscz <mtk.eworkers@gmail.com>
2013-06-17 15:09:35 +0000

    Prvni nahozeni

---
+++ src/anagram/Anagram.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package anagram;
+
+import java.util.*;
+
+/**
+ *
+ * @author matejka
+ */
+public class Anagram
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        Mapa mapicka = new Mapa();
+        Anagram anagram = new Anagram();
+        Boolean mameAnagram = false;
+        
+        System.out.print(mapicka + "\n");
+        
+        mapicka.vypisMapu();
+        
+        mapicka.pridejVyskyt(new Character('b'));
+        mapicka.pridejVyskyt(new Character('b'));
+        mapicka.pridejVyskyt(new Character('b'));
+        mapicka.pridejVyskyt(new Character('b'));
+        mapicka.pridejVyskyt(new Character('b'));
+        mapicka.pridejVyskyt(new Character('b'));
+        
+        mapicka.vypisMapu();
+        mameAnagram = anagram.zjistiAnagram("cadac", "daacc");
+        
+        
+        
+    }
+    
+    public static Boolean zjistiAnagram(String prvniVeta, String druhaVeta)
+    {
+        Boolean jeAnagram = false;
+        
+        Character pismeno;
+        Character pismeno2;
+        
+        Mapa mapkaPrvnihoSlova = new Mapa();
+        
+        for (int i = 0; i < prvniVeta.length(); i++)
+        {
+            pismeno = prvniVeta.charAt(i);
+            mapkaPrvnihoSlova.pridejVyskyt(pismeno);
+        }
+        
+        mapkaPrvnihoSlova.vypisMapu();
+
+        Mapa mapkaDruhehoSlova = new Mapa();
+        
+        for (int j = 0; j < druhaVeta.length(); j++)
+        {
+            pismeno2 = druhaVeta.charAt(j);
+            mapkaDruhehoSlova.pridejVyskyt(pismeno2);
+        }
+        
+        mapkaDruhehoSlova.vypisMapu();
+        
+        
+        return Mapa.porovnejMapy(mapkaPrvnihoSlova, mapkaDruhehoSlova);
+        
+        
+        
+        
+    }
+}
---
+++ src/anagram/Mapa.java
@@ -0,0 +1,73 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package anagram;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author matejka
+ */
+public class Mapa
+{
+    private Map mapa = new HashMap();
+    public Mapa()
+    {
+    
+        mapa.put('a', 0);
+        mapa.put('b', 0);
+        mapa.put('c', 0);
+        mapa.put('d', 0);
+        mapa.put('e', 0);
+        mapa.put('f', 0);
+        mapa.put('g', 0);
+        mapa.put('h', 0);
+        mapa.put('i', 0);
+        mapa.put('j', 0);
+        mapa.put('k', 0);
+        mapa.put('l', 0);
+        mapa.put('m', 0);
+        mapa.put('n', 0);
+        mapa.put('o', 0);
+        mapa.put('p', 0);
+        mapa.put('q', 0);
+        mapa.put('r', 0);
+        mapa.put('s', 0);
+        mapa.put('t', 0);
+        mapa.put('u', 0);
+        mapa.put('v', 0);
+        mapa.put('w', 0);
+        mapa.put('x', 0);
+        mapa.put('y', 0);
+        mapa.put('z', 0);
+     }   
+    
+    public void pridejVyskyt(Character klic)
+    {
+        Object puvodniHodnota;
+        puvodniHodnota = mapa.get(klic);
+        mapa.put(klic, (Integer)puvodniHodnota+1);
+        
+    }
+    
+    public void vypisMapu()
+    {
+        System.out.print(mapa + "\n");
+    }
+        
+    public Boolean porovnejMapy(HashMap mapa1, HashMap mapa2)
+    {
+        Boolean stejne = false;
+        
+        if(mapa1.equals(mapa2))
+        {
+            stejne = true;
+        }
+        
+        return stejne;
+    }
+    
+}
